ff709fc34ce8ce7ae00bb5763ecba9b85986319a
CacahueteSansSel <24205659+CacahueteSansSel@users.noreply.github.com>
2026-01-28 19:54:54 +0100

    more work on ui + improved dates

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import {useEffect, useRef, useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import BeaconIcon from './assets/beacon-icon.svg'
 import './App.css'
 import {apiGetFeed} from "./api/api.js";
 import {Article} from "./components/Article.jsx";
@@ -59,9 +58,11 @@ function App() {
 
     return (
         <div className={"mx-4 my-2 mb-20 mt-10"}>
-            <p className={"text-2xl font-bold text-center"}>Beacon</p>
-            <p className={"text-2xl text-center"}>Articles récents</p>
-            <div className={"flex flex-col gap-6 mt-4"}>
+            <div className={"flex flex-row justify-between items-center"}>
+                <img src={BeaconIcon} alt={"Beacon Logo"} className={"h-20"}/>
+                <p className={"text-xl font-bold mt-5 mr-7"}>Actualités</p>
+            </div>
+            <div className={"flex flex-col gap-8 mt-4"}>
                 {feed.length > 0 && feed.map((item, i) => {
                     return <Article key={i} article={item} />
                 })}
--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,15 +1,32 @@
-﻿export function Article({article}) {
+﻿import DefaultCover from '../assets/default-cover.png'
+
+export function Article({article}) {
     let date = new Date(article.publishDate)
 
     let fgColor = article.outlet.color === "000000" ? "FFFFFF" : "000000"
+    let isToday = date.toDateString() === new Date().toDateString()
+
+    function getArticleCategoryTitle(category) {
+        switch (category) {
+            case "general":
+                return "Général"
+            case "tech":
+                return "Technologie"
+            case "apple":
+                return "Apple"
+        }
+    }
 
     return (
-        <div className={"border-gray-300 relative border p-4 rounded-lg shadow-2xl flex flex-col gap-1"} onClick={() => window.open(article.url)}>
-            <div className={"absolute rounded px-2 -translate-y-6 font-bold border-gray-300 border"} style={{backgroundColor: "#" + article.outlet.color, color: "#" + fgColor}}>{article.outlet.title}</div>
-            <div className={"flex flex-row gap-2"}>
-                <p className={"opacity-50"}>Publié le {date.toLocaleDateString()} à {date.toLocaleTimeString()}</p>
+        <div style={{borderColor: "#" + article.outlet.color}} className={"relative border-2 p-4 rounded-lg shadow-2xl flex flex-col gap-1"} onClick={() => window.open(article.url)}>
+            <div style={{borderColor: "#" + article.outlet.color}} className={"absolute rounded px-2 -translate-y-6 font-bold border-2 bg-white"} style={{color: "#" + article.outlet.color}}>{article.outlet.title}</div>
+            <div className={"flex flex-row justify-between items-center"}>
+                {!isToday && <p className={"opacity-50"}>le {date.toLocaleDateString()}, à {date.getHours()}h{date.getMinutes().toString().padStart(2, '0')}</p>}
+                {isToday && <p className={"opacity-50"}>aujourd'hui, à {date.getHours()}h{date.getMinutes().toString().padStart(2, '0')}</p>}
+                <p className={"opacity-50 font-semibold"}>{getArticleCategoryTitle(article.outlet.category)}</p>
             </div>
-            <img className={"h-50 w-full object-cover"} src={article.coverImageUrl}/>
+            {article.coverImageUrl && <img className={"h-50 w-full object-cover border-2"} style={{borderColor: "#" + article.outlet.color}} src={article.coverImageUrl}/>}
+            {!article.coverImageUrl && <img className={"h-50 w-full object-cover"} src={DefaultCover}/>}
             <p className={"text-xl font-bold"}>{article.title}</p>
         </div>
     )
